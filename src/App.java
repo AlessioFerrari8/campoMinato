092f9f59d556d003084fae44327b6dec5c82f245
alessio.ferrari <alessio.ferrari@marconirovereto.it>
2025-11-13 11:53:37 +0100

    Feat: add JFrame setup for Minesweeper game

--- src/App.java
+++ src/App.java
@@ -1,5 +1,18 @@
+import javax.swing.JFrame;
+
 public class App {
     public static void main(String[] args) throws Exception {
+        JFrame f = new JFrame("Minesweeper");
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // crea campo
         
+
+        // aggiungi il campo
+
+
+        f.setSize(466, 342);
+        f.setLocationRelativeTo(null);
+        f.setVisible(true);
     }
 }
